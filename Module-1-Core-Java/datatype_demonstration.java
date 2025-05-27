8793644987004ce263b32ea2729d6dd71fd4fdff
rishi27nukala <rishitanukala@gmail.com>
2025-05-27 16:34:44 +0530

    exercise6

---
+++ Module-1-Core-Java/datatype_demonstration.java
@@ -0,0 +1,14 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int myInt = 100;
+        float myFloat = 10.5f;
+        double myDouble = 123.456;
+        char myChar = 'A';
+        boolean myBool = true;
+        System.out.println("int: " + myInt);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + myBool);
+    }
+}
