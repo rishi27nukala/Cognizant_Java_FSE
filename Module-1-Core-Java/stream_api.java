bbf10ebe7a0d200464179da0b881847ccf28edac
rishi27nukala <rishitanukala@gmail.com>
2025-05-27 18:07:57 +0530

    exercise28

---
+++ Module-1-Core-Java/stream_api.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class EvenNumbersStream {
+    public static void main(String[] args) {
+        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> evens = num.stream()
+                                     .filter(n -> n % 2 == 0)
+                                     .collect(Collectors.toList());
+        System.out.println("Even numbers: " + evens);
+    }
+}
