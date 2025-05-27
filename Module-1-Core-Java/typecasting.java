04749a3e80db8d10b727e808cf0f7a70184db423
rishi27nukala <rishitanukala@gmail.com>
2025-05-27 16:35:58 +0530

    exercise7

---
+++ Module-1-Core-Java/typecasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double d = 9.78;
+        int i = (int) d; 
+        System.out.println("Double to int: " + i);
+        int j = 5;
+        double x = j; 
+        System.out.println("Int to double: " + x);
+    }
+}
